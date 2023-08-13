0235e9f1f2b216ca60dd2d246e681e826eda6e35
Bartosz Szczygiel <eziosoft@gmail.com>
2023-08-14 01:55:05 +0200

    web

---
+++ web.py
@@ -0,0 +1,54 @@
+from flask import Flask, Response, render_template
+import os
+import cv2
+from urls import *
+from utils.FPSCounter import FPSCounter
+from yolo_onnx.YOLOv7 import YOLOv7_onnx
+
+app = Flask(__name__)
+
+# Initialize global variables
+fps = FPSCounter()
+os.environ['DISPLAY'] = ':0.0'
+cap = cv2.VideoCapture(url1)
+onnxModel_path = "models/yolov7/best.onnx"
+yolo7_onnx = YOLOv7_onnx(onnxModel_path, classes=["boat", "patio", "people"])
+
+def generate_frames():
+    while cap.isOpened():
+        success, frame = cap.read()
+
+        if success:
+            fps.update()
+
+            detections, ratio, dwdh = yolo7_onnx.detect(frame)
+            filter_iou = yolo7_onnx.non_max_suppression(detections, 0.9)
+            annotated_frame = yolo7_onnx.drawDetections(filter_iou, frame, ratio, dwdh, filter_classs=None)
+
+            i = 0
+            for a in filter_iou:
+                if int(a[5]) == 2:
+                    i += 1
+
+            print(f'FPS = %.2f, people = %d' % (fps.getFPS(), i ), end='\r')
+            annotated_frame = cv2.putText(annotated_frame, "Number of people: " + str(i), (50, 50),
+                                          cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+            _, buffer = cv2.imencode('.jpg', annotated_frame)
+            frame_bytes = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+        else:
+            break
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
